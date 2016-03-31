a212c5f05bfd0f73ee7c9e9a2773272062da20cb
Barbaros Yıldırım <barbarosaliyildirim@gmail.com>
2016-04-01 00:13:07 +0300

    send message operation handled, quit and send buttons placed and events applied.

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ class SendButton(npyscreen.MiniButtonPress):
     def whenPressed(self):
         pass
 
+
 class ChatBox(npyscreen.BoxTitle):
     _contained_widget = npyscreen.Textfield
 
@@ -31,29 +32,33 @@ class CustomPager(npyscreen.Pager):
         self._values_cache_for_wrapping = []
         self.widgets_inherit_color = True
         self.color = 'DEFAULT'
+        self.lines_placed = False
 
     def _wrap_message_lines(self, message_lines, line_length):
         lines = []
-        for line in message_lines:
-            if line.rstrip() == '':
-                lines.append('')
-            else:
-                if line.find('\n\t') != -1:
-                    user_info, message_text = line.rsplit("\n\t", 1)
-                    space = line_length - 1 - len(user_info)
-                    name, timestamp = user_info.split('(')
-                    message_header = "{}{}({}".format(name.strip(), '.' * space,
-                                                      timestamp.strip())
-                    lines.append("->{}".format(message_header))
-                else:
-                    message_text = line
-                this_line_set = list(map(
-                    lambda x: "\t\t\t\t{}".format(x),
-                    textwrap.wrap(message_text.rstrip(), line_length - 5)))
-                if this_line_set:
-                    lines.extend(this_line_set + [''])
-                else:
+        if not self.lines_placed:
+            for line in message_lines:
+                if line.rstrip() == '':
                     lines.append('')
+                else:
+                    if line.find('\n\t') != -1:
+                        user_info, message_text = line.rsplit("\n\t", 1)
+                        space = line_length - 1 - len(user_info)
+                        name, timestamp = user_info.split('(')
+                        message_header = "{}{}({}".format(name.strip(), '.' * space,
+                                                          timestamp.strip())
+                        lines.append("->{}".format(message_header))
+                    else:
+                        message_text = line
+                    this_line_set = list(map(
+                        lambda x: "\t\t\t\t{}".format(x),
+                        textwrap.wrap(message_text.rstrip(), line_length - 5)))
+                    if this_line_set:
+                        lines.extend(this_line_set + [''])
+                    else:
+                        lines.append('')
+        else:
+            lines = message_lines
         return lines
 
     def display_value(self, vl):
@@ -74,12 +79,31 @@ class HistoryBox(npyscreen.BoxTitle):
     _contained_widget = CustomPager
 
 
-class PyGramForm(npyscreen.ActionForm):
+class PyGramForm(npyscreen.ActionFormExpanded):
+    CANCEL_BUTTON_BR_OFFSET = (5, 12)
+    OK_BUTTON_TEXT = "QUIT"
+    CANCEL_BUTTON_TEXT = "SEND"
     form_width = 30
     full_name = '{} {}'.format(TG.sender.get_self().first_name, TG.sender.get_self().last_name)
 
     def on_ok(self):
-        self.parentApp.switchForm(None)
+        ans = npyscreen.notify_yes_no('Are you sure, you want to quit?')
+        if ans:
+            self.parentApp.switchForm(None)
+
+    def on_cancel(self):
+        """ Message will be send """
+        if self.dialog_list.entry_widget:
+            selected_index = self.dialog_list.entry_widget.value
+            dialog_name = self.dialog_list.values[selected_index]
+            text = self.chat_box.entry_widget.value.strip()
+            if text:
+                send_status = TG.sender.send_msg(dialog_name, text)
+                if send_status:
+                    self.chat_box.entry_widget.value = ""
+                    self.load_history()
+        else:
+            npyscreen.notify_ok_cancel('Please select receiver first.')
 
     def create(self):
         self.dialog_list = self.add(npyscreen.BoxTitle, name="Dialog List", scroll_exit=True,
@@ -94,7 +118,6 @@ class PyGramForm(npyscreen.ActionForm):
 
         self.chat_box = self.add(ChatBox, name='{}'.format(self.full_name), scroll_exit=True,
                                  editable=True, max_height=5, contained_widget_arguments={'name': ' '})
-        self.but = self.add(SendButton, name='SEND')
 
     def load_history(self, *args, **keywords):
         selected_index = self.dialog_list.entry_widget.value
@@ -102,6 +125,7 @@ class PyGramForm(npyscreen.ActionForm):
         selected_dialog = list(filter(
             lambda x: x.print_name == printed_name, self.parentApp.dialog_list))
         if selected_dialog:
+            self.chat_history.entry_widget.lines_placed = False
             self.chat_history.name = (getattr(selected_dialog[0], 'title', None) or
                                       "{} {}".format(getattr(selected_dialog[0], 'first_name', ''),
                                                      getattr(selected_dialog[0], 'last_name', '')))
@@ -116,6 +140,8 @@ class PyGramForm(npyscreen.ActionForm):
                                                       getattr(getattr(x, 'media', DictObject()), 'address', '')))),
                            TG.sender.history(printed_name, 100, 0, retry_connect=True))))
             self.parentApp.fill_history()
+            self.find_next_editable()
+            self.chat_history.entry_widget.lines_placed = True
 
         # Force movement to history box
         self.find_next_editable()
@@ -143,11 +169,9 @@ class PyGramApp(npyscreen.NPSAppManaged):
         self.contacts_list = TG.sender.contacts_list()
         self.addForm('MAIN', PyGramForm, name='Welcome PyGram')
 
-    def onCleanExit(self):
-        npyscreen.notify_wait("Goodbye!")
-
     def fill_history(self):
         self.resetHistory()
+
 
 if __name__ == "__main__":
     logging.basicConfig(filename="./log/pygram-{}.log".format(datetime.now()))
